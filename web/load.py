b1b84b57b344afc23bdef5386d3eb099ac67b770
Jakob Klepp <jklepp@student.tgm.ac.at>
2015-01-09 10:29:18 +0100

    fixed check if self.path exist

--- web/load.py
+++ web/load.py
@@ -23,40 +23,43 @@ class LoadHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         if hasattr(self, 'path'):
             path = self.path
-            if path in paths.keys():
-                paths[path].do_GET(self)
+        else:
+            path = None
+
+        if path in paths.keys():
+            paths[path].do_GET(self)
 
         else:
             message_parts =  """<!doctype html public>
-                                <html>
-                                <head>
-                                    <title>Welcome to Server """ + sys.argv[1] + """!</title>
-                                    <style>
-                                        body {
-                                            -webkit-animation: myfirst 5s; /* Chrome, Safari, Opera */
-                                            animation: myfirst 5s;
-                                            background: yellow;}
-                                        /* Chrome, Safari, Opera */
-                                        @-webkit-keyframes myfirst {
-                                            from {background: red;}
-                                            to {background: yellow;}}
-                                        /* Standard syntax */
-                                        @keyframes myfirst {
-                                            from {background: red;}
-                                            to {background: yellow;}} 
-                                    </style>
-                                </head>
-                                <body>
-                                <div id="wrapper" style="width: 100%; margin: auto auto auto auto;">
-                                    <marquee direction="down" width="100%" height="50%" behavior="alternate" >
-                                        <marquee behavior="alternate">
-                                            <h1>Welcome to Server """ + sys.argv[1] + """!</h1>
-                                        </marquee>
-                                    </marquee>
-                                </div>
-                                
-                                </body>
-                                </html>"""
+            <html>
+            <head>
+                <title>Welcome to Server """ + sys.argv[1] + """!</title>
+                <style>
+                    body {
+                        -webkit-animation: myfirst 5s; /* Chrome, Safari, Opera */
+                        animation: myfirst 5s;
+                        background: yellow;}
+                    /* Chrome, Safari, Opera */
+                    @-webkit-keyframes myfirst {
+                        from {background: red;}
+                        to {background: yellow;}}
+                    /* Standard syntax */
+                    @keyframes myfirst {
+                        from {background: red;}
+                        to {background: yellow;}} 
+                </style>
+            </head>
+            <body>
+            <div id="wrapper" style="width: 100%; margin: auto auto auto auto;">
+                <marquee direction="down" width="100%" height="50%" behavior="alternate" >
+                    <marquee behavior="alternate">
+                        <h1>Welcome to Server """ + sys.argv[1] + """!</h1>
+                    </marquee>
+                </marquee>
+            </div>
+            
+            </body>
+            </html>"""
 
             message_parts = message_parts.split('\n')
 
